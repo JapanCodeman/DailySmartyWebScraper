62220fa9ace1aa87ded7d97f3bbf449fa497b0a5
JapanCodeman <ryansurdick@MacBook-Pro.mshome.net>
2021-09-15 10:45:29 +0900

    Outline of project requirements

---
+++ main.py
@@ -0,0 +1,16 @@
+'''
+Build a web scraping algorithm that pulls data from http://www.dailysmarty.com/topics/python
+
+Should return the various post links in a normal English format
+
+For example:
+
+
+'Installing Anaconda Python Data Science Platform'
+
+Suggested modules to use:
+
+- requests
+- inflection
+- beautifulsoup4
+'''
