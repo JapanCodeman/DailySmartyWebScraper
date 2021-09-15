da4efae08c4e4d6ec8139f7235ac78f71f8e2e8e
JapanCodeman <ryansurdick@MacBook-Pro.local>
2021-09-15 21:31:23 +0900

    Installed, imported modules, started code

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-'''
+"""
 Build a web scraping algorithm that pulls data from http://www.dailysmarty.com/topics/python
 
 Should return the various post links in a normal English format
@@ -13,4 +13,22 @@ Suggested modules to use:
 - requests
 - inflection
 - beautifulsoup4
-'''
+"""
+
+import requests
+import inflection
+import bs4 as bs
+import pprint as pprint
+import lxml
+
+source = requests.get('http://www.dailysmarty.com/topics/python').text
+
+soup = bs.BeautifulSoup(source, 'lxml')
+body = soup.body
+
+post_item = soup.find_all('a', class_='post-link-title')
+
+post = post_item
+print(post)
+
+links = []
